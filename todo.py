cf4ed46ae2749f53cb629cf5578e051558b62abd
initaman <amandabral2104@gmail.com>
2025-12-29 18:11:53 +0530

    feat(todo): Add an error handling for empty todo list

--- todo.py
+++ todo.py
@@ -10,8 +10,16 @@ async def add_todo(todo):
     todo_list.append(todo)
     return {"Message": "Todo item has been added successfully."}
 
+
 # Create a GET method to retrieve all items from the todo_list
 @todo_router.get("/todos")
 async def get_todos():
-    return todo_list
+    try:
+        # Check if the todo_list is empty or not
+        if len(todo_list) == 0:
+         return {"Message": "Todo list is empty. Please add a item to see the todo lists"}
+        else:
+           return todo_list
+    except Exception as e:
+       return f"{e}"
 
