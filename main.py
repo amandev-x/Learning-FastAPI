784775c7e999abc898a5716f0b4d86256c0f4288
initaman <amandabral2104@gmail.com>
2025-12-29 17:51:42 +0530

    feat(todo): Add include_router method

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from fastapi import FastAPI
+from todo import todo_router
 
-# app = FastAPI()
-router = APIRouter()
+app = FastAPI()
 
-@router.get("/")
+"""
+Incuded include_router().
+Both FastAPI and APIRouter works in same way but uvicorn cannot use APIRouter instance to server the application.
+It uses FastAPI to serve the applicatio
+But why include_route()?
+This is responsible for adding routes defined in the APIRouter class in the project to the main application
+"""
+app.include_router(todo_router)
+
+@app.get("/")
 async def welcome():
     return {"message": "Welcome to the FastAPI application!"}
+
