19273d017219d8d489c38c595ef3222c735f5f80
initaman <amandabral2104@gmail.com>
2025-12-31 21:10:04 +0530

    feat(todo): Add Path to get more granular control

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from models import Todo
+from fastapi import APIRouter, Path, HTTPException
+from models import Todo, TodoResponse, TodoResponseList
 todo_router = APIRouter()
 
 # Create Temporary in-app database alongside two routes for the additional and retrieval of todo items.
@@ -13,25 +13,32 @@ async def add_todo(todo: Todo) -> dict:
 
 
 # Create a GET method to retrieve all items from the todo_list
-@todo_router.get("/todos")
-async def get_todos() -> dict:
-    try:
-        # Check if the todo_list is empty or not
-        if len(todo_list) == 0:
-         return {"Message": "Todo list is empty. Please add a item to see the todo lists"}
-        else:
-           return {"todos": todo_list}
-    except Exception as e:
-       return f"{e}"
+# Not using dict beacause we are using response_model to define the response structure
+@todo_router.get("/todos", response_model=TodoResponseList)
+async def get_todos():
+    if len(todo_list) == 0:
+        raise HTTPException(
+            status_code=200,
+            detail="Todo list is empty. Please add a item to see the todo lists"
+        )
+    else:
+        return {"todos": todo_list}
     
-# Create a GET method to retrieve a specific todo item by its id
-@todo_router.get("/todo/{todo_id}")
-async def get_todo_by_id(todo_id: int) -> dict:
-    try:
-        for todo in todo_list:
-            if todo.id == todo_id:
-                return {"todo": todo}
-            return {"Message": "Todo item not found."}
-    except Exception as e:
-        return f"{e}"
-
+"""
+Create a GET method to retrieve a specific todo item by its id.
+In this method, we are using Path to validate the todo_id parameter. In simple words, we are adding some additional
+validation to the todo_id parameter.
+This adds better documentation and validation to the Swagger UI and Redoc documentation.
+In Path(...), ... means that this parameter is required.
+"""
+@todo_router.get("/todo/{todo_id}", response_model=TodoResponse)
+async def get_todo_by_id(todo_id: int = Path(..., description="The ID of the todo item to retrieve", gt=0, example=1)):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {"todo": todo}
+       
+    # If the item didnt found then raise HTTPException error
+    raise HTTPException(
+       status_code=404,
+       detail="Todo item not found"
+       )
